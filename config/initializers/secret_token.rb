3772709e7ff004cda4e7f32e8448a303e67349f1
root <world.mover@gmail.com>
2012-04-07 15:16:04 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppb::Application.config.secret_token = '22caae9852182d3d5b1eaab9bb887a85ad89629c91eab4ad24ca17c0c51aea41d676af1a2ff245f60d99b3afb9704be23b118114ac1b9dd75affafcff568eb67'
